eca5599b1907c3bd01bd44d9960ea6d4172e3dc6
alexcattann27 <acattan2701@gmail.com>
2025-12-07 16:24:26 +0100

    Update IntersectionSimu.java

--- src/Simulation/IntersectionSimu.java
+++ src/Simulation/IntersectionSimu.java
@@ -4,7 +4,6 @@ public class IntersectionSimu {
     public String id;
     public int x, y;
 
-    // C'est cette variable qui manquait :
     public boolean estVisite = false;
 
     public IntersectionSimu(String id, int x, int y) {
