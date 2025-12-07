c5cf53f164a456a49fc19c3167f36d6768f20106
rubensadoun3 <rubensadoun3@gmail.com>
2025-12-07 19:07:36 +0100

    projet tdg

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,11 @@ import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
+        /*Pour que le programme fonctionne :
+        -run GenerateurGPS_Secours (GenerateurGPS ne fonctionne pas et n'arrive pas à
+        transmettre les données à vitry_gps.csv)
+        -run Main
+         */
 
         System.out.println(" PROJET COLLECTE DÉCHETS AVEC CAMIONS");
         Graphe graphe = LecteurCSV.lireGraphe("data/vitry_gps.csv");
