83554942dc1138eed06104254a69f273575b9e36
alexcattann27 <acattan2701@gmail.com>
2025-12-07 16:08:41 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("=== PROJET COLLECTE DÉCHETS ===");
 
-        // ATTENTION : On charge le fichier généré à l'étape 1
+        //On charge le fichier généré à l'étape 1
         Graphe graphe = LecteurCSV.lireGraphe("data/vitry_gps.csv");
 
         SwingUtilities.invokeLater(() -> {
